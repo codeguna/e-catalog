5e71133bf1d2488b1df346a36ff11d7b3a3b17e7
codeguna <pratamagunadhi@gmail.com>
2025-07-21 16:02:16 +0700

    [FEAT] Changes IMG Slides

--- resources/views/frontend/layouts/spinner.blade.php
+++ resources/views/frontend/layouts/spinner.blade.php
@@ -10,14 +10,14 @@
                 <div id="carouselId" class="carousel slide position-relative" data-bs-ride="carousel">
                     <div class="carousel-inner" role="listbox">
                         <div class="carousel-item active rounded">
-                            <img src="{{ asset('landing/img/hero-img-1.png') }}"
+                            <img src="{{ asset('landing/img/hero-img-1.jpg') }}"
                                 class="img-fluid w-100 h-100 bg-secondary rounded" alt="First slide">
-                            <a href="#" class="btn px-4 py-2 text-white rounded">Fruites</a>
+                            {{-- <a href="#" class="btn px-4 py-2 text-white rounded"></a> --}}
                         </div>
                         <div class="carousel-item rounded">
                             <img src="{{ asset('landing/img/hero-img-2.jpg') }}" class="img-fluid w-100 h-100 rounded"
                                 alt="Second slide">
-                            <a href="#" class="btn px-4 py-2 text-white rounded">Vesitables</a>
+                            {{-- <a href="#" class="btn px-4 py-2 text-white rounded"></a> --}}
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselId"
