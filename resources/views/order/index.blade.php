343e6ca1077a5eab1a2c1ed7975eba96a429e998
codeguna <pratamagunadhi@gmail.com>
2025-07-21 21:54:47 +0700

    [FIX] on relation null

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -61,7 +61,7 @@
                                                 @foreach ($order->orderItems as $item)
                                                     <ol>
                                                         <li>{{ $item->product->name??'' }} | Rp.
-                                                            {{ number_format($item->product->price) }}</li>
+                                                            {{ number_format($item->product->price)??'' }}</li>
                                                     </ol>
                                                 @endforeach
                                             </td>
