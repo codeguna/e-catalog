2e824ed39ecbff8916bdf8b5d14105e5d5fdc512
fahmirezaf <fahmirferdiansyah@gmail.com>
2025-07-21 18:27:49 +0700

    [FIX] add comment

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Gate;
  */
 class OrderController extends Controller
 {
-    /***
+    /****
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
